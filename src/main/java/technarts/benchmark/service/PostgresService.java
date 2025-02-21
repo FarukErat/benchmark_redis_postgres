78bde8946c4133dfe6cd07e9ce6e9195025c3f1e
farukerat <farukerat1327@gmail.com>
2025-02-21 20:40:56 +0300

    add parameters

--- src/main/java/technarts/benchmark/service/PostgresService.java
+++ src/main/java/technarts/benchmark/service/PostgresService.java
@@ -1,4 +1,11 @@
 package technarts.benchmark.service;
 
 public class PostgresService {
+    private static final String HOST = "localhost";
+    private static final String PORT = "5432";
+    private static final String DATABASE = "mydatabase";
+    private static final String USER = "myuser";
+    private static final String PASSWORD = "mypassword";
+    private static final String JDBC_URL = "jdbc:postgresql://" + HOST + ":" + PORT + "/" + DATABASE;
+
 }
