17b166393845ce7f2a36c947ea9807e76c8365ce
farukerat <farukerat1327@gmail.com>
2025-02-21 21:27:59 +0300

    return jedis

--- src/main/java/technarts/benchmark/service/RedisService.java
+++ src/main/java/technarts/benchmark/service/RedisService.java
@@ -6,17 +6,5 @@ public class RedisService {
     private static final String HOST = "localhost";
     private static final int PORT = 6379;
 
-    private static final Jedis jedis = new Jedis(HOST, PORT);
-
-    public static String get(String key) {
-        return jedis.get(key);
-    }
-
-    public static String set(String key, String value) {
-        return jedis.set(key, value);
-    }
-
-    public static Long del(String... keys) {
-        return jedis.del(keys);
-    }
+    public static final Jedis jedis = new Jedis(HOST, PORT);
 }
