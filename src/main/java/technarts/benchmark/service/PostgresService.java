5ced7843b8b6b4d5be5b157ca3ce09a3b5a36b7d
farukerat <farukerat1327@gmail.com>
2025-02-21 21:12:55 +0300

    make port integer

--- src/main/java/technarts/benchmark/service/PostgresService.java
+++ src/main/java/technarts/benchmark/service/PostgresService.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 
 public class PostgresService {
     private static final String HOST = "localhost";
-    private static final String PORT = "5432";
+    private static final int PORT = 5432;
     private static final String DATABASE = "mydatabase";
     private static final String USER = "myuser";
     private static final String PASSWORD = "mypassword";
